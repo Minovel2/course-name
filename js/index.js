4e241fb388545eec3fe4ed3cb229d5037ef29ecf
Minovel2 <kekovdimon@gmail.com>
2024-09-10 16:17:30 +0300

    fixes

--- js/index.js
+++ js/index.js
@@ -3,21 +3,3 @@ let login = document.querySelector('#password')
 let repPassword = document.querySelector('#repeatPassword')
 let submit = document.querySelector('#submit')
 
-var fs = require(‘fs’);
-
-function fileHandler(){
-
-  fs.open('testFile.txt', 'w', (err) => {
-      if(err) throw err;
-      console.log('File created');
-  });
-  
-}
-
-function User(name, login, repPassword){
-  this.login = login;
-  fs.appendFile('testFile.txt', this.login, (err) => {
-    if(err) throw err;
-    console.log('Data has been added!');
-});
-}
